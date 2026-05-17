8464e24b7d48958a1e5ab54407a9d004190d43c2
Samprada07 <samprada2058@gmail.com>
2026-05-18 00:45:57 +0545

    feat: add dark mode support

--- shared/src/commonMain/kotlin/com/example/bookapp/App.kt
+++ shared/src/commonMain/kotlin/com/example/bookapp/App.kt
@@ -1,9 +1,18 @@
 package com.example.bookapp
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import com.example.bookapp.presentation.navigation.NavGraph
 
 @Composable
 fun App() {
-    NavGraph()
+    val darkTheme = isSystemInDarkTheme()
+    MaterialTheme(
+        colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme()
+    ) {
+        NavGraph()
+    }
 }
